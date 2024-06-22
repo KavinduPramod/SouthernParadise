0bad8eeccd77b8230ed1f5fc8e7233c2ee7fc526
Kavindu Pramod <kavindu.20210410@iit.ac.lk>
2024-06-23 02:48:41 +0530

    chore: Add bootstrap-icons npm dependency for icon support

---
+++ src/components/footer.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="container-fluid bg-dark text-light py-5">
+      <footer className="container">
+        <div className="row">
+          <div className="col-12 col-md-3 mb-3">
+            <h5>Quick Links</h5>
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <Link to="/" className="nav-link p-0 text-light">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item mb-2">
+                <Link to="/about" className="nav-link p-0 text-light">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item mb-2">
+                <Link to="/contact" className="nav-link p-0 text-light">
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item mb-2">
+                <Link to="/land" className="nav-link p-0 text-light">
+                  Land
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-12 col-md-3 mb-3">
+            <h5>More Locations</h5>
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <a
+                  href="https://en.wikipedia.org/wiki/Matara_fort"
+                  className="nav-link p-0 text-light"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Matara Fort
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="https://en.wikipedia.org/wiki/Matara,_Sri_Lanka"
+                  className="nav-link p-0 text-light"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Paravi Duwa Temple
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="https://en.wikipedia.org/wiki/Star_Fort,_Matara"
+                  className="nav-link p-0 text-light"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Star Fort
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="https://www.google.com/maps/place/Madiha+Beach,+Beach+Road,+Madiha,+Matara,+Sri+Lanka.,+Matara+81000/data=!4m2!3m1!1s0x3ae13ee42b00bf13:0xaa62b86b03d9ed9a?sa=X&ved=1t:242&ictx=111"
+                  className="nav-link p-0 text-light"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Madiha Beach
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-12 col-md-4 mb-3">
+            <h5>Contact Us</h5>
+            <p>
+              <i className="bi bi-telephone-fill"></i> +94 76 813 7825
+            </p>
+            <p>
+              <i className="bi bi-envelope-fill"></i> 0Pjv6@example.com
+            </p>
+            <div>
+              <a href="https://www.facebook.com" className="text-light me-3">
+                <i className="bi bi-facebook"></i>
+              </a>
+              <a href="https://www.twitter.com" className="text-light me-3">
+                <i className="bi bi-twitter"></i>
+              </a>
+              <a href="https://www.instagram.com" className="text-light me-3">
+                <i className="bi bi-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com" className="text-light">
+                <i className="bi bi-linkedin"></i>
+              </a>
+            </div>
+          </div>
+
+          <div className="col-12 mt-3">
+            <p>
+              Meet the developer click{" "}
+              <a
+                href="https://kavindupramod.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-light"
+              >
+                here
+              </a>
+              !
+            </p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,23 +2,72 @@ import React from "react";
 import Navbar from "../components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Hero from "../components/hero";
+import Footer from "../components/footer";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Home = () => {
   return (
-    <div className="min-vh-100 bg-black">
+    <div className="min-vh-100 bg-black text-light">
       <Navbar />
-      <div className="min-vh-100 ">
+      <div className="container-fluid">
         <Hero />
-        <div>
-          <h1>home</h1>
-
-          <p>home</p>
-
-          <p>home</p>
-
-          <p>home</p>
+        <div className="m-5 text-light fs-5 text-center">
+          <p className="fs-5">
+            Prime land for sale in Matara ancient Holland Fort. 13.5 perches of
+            pristine land For sale in the heart of Matara historic Holland Fort,
+            Southern Province, Sri Lanka. This stunning property offers
+            breathtaking views of the Nilwala River, providing a serene and
+            picturesque setting.
+          </p>
+          <h4 className="text-light fw-bold text-center mt-4 mb-3">
+            Prime Location Benefits
+          </h4>
+          <ul className="list-unstyled">
+            <li className="my-2">
+              <i className="bi bi-geo-alt-fill text-primary"></i> Just minutes
+              away from the beautiful beach
+            </li>
+            <li className="my-2">
+              <i className="bi bi-bank2 text-success"></i> Close to banks and
+              essential services
+            </li>
+            <li className="my-2">
+              <i className="bi bi-bus-front-fill text-warning"></i> Conveniently
+              near the bus stand for easy transportation
+            </li>
+            <li className="my-2">
+              <i className="bi bi-cart-fill text-danger"></i> Proximity to
+              Keells and Cargills Super Markets for all your shopping needs
+            </li>
+            <li className="my-2">
+              <i className="bi bi-hospital-fill text-info"></i> Accessible to
+              both government and private hospitals for quality healthcare
+            </li>
+          </ul>
+          <p className="fs-5 mt-4">
+            The property is located in the heart of Matara. Ideal for both
+            residential and tourism purposes, this land is a rare opportunity to
+            own a piece of paradise. Don't miss out on making your dream a
+            reality!
+          </p>
+          <div className="card bg-dark border-light mt-5">
+            <div className="card-body">
+              <h5 className="card-title text-light">Location Map</h5>
+              <iframe
+                title="Matara Holland Fort Location"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63387.47692775094!2d80.53417964268846!3d5.948530016012054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae132a3a70de621%3A0x42a46c00bdc679c8!2sMatara%2C%20Sri%20Lanka!5e0!3m2!1sen!2sus!4v1657903001732!5m2!1sen!2sus"
+                width="100%"
+                height="400"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
