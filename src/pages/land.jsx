9b1479fde8d113af4a13450452b3c9b10cb2267c
Kavindu Pramod <kavindu.20210410@iit.ac.lk>
2024-06-26 00:31:07 +0530

    feat: Add image gallery with lightbox functionality

--- src/pages/land.jsx
+++ src/pages/land.jsx
@@ -1,14 +1,72 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import Navbar from "../components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "../components/footer";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import BeachImage from "../assets/matara-beach.jpg";
+import BoatRideImage from "../assets/nilwala-boat.jpg";
+import ParaviDuwaImg from "../assets/paraviDuwa.jpg";
+import PropertyImage from "../assets/property.jpeg";
 
 const land = () => {
+  const photos = [
+    {
+      src: BeachImage,
+      width: 4,
+      height: 3,
+    },
+    {
+      src: BoatRideImage,
+      width: 4,
+      height: 3,
+    },
+    {
+      src: ParaviDuwaImg,
+      width: 4,
+      height: 3,
+    },
+    {
+      src: PropertyImage,
+      width: 4,
+      height: 3,
+    },
+  ];
+
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
-    <div>
+    <div className="bg-black text-light min-vh-100">
       <Navbar />
-      <div>
-        <h1>land</h1>
+      <div className="container-fluid">
+        <Gallery photos={photos} onClick={openLightbox} />
       </div>
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map((x) => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+      <Footer />
     </div>
   );
 };
